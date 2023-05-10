5c02d94c7ea852d9cbeea7e3922b2ba432d2ac63
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-09 23:58:49 -0300

    Classe Program e Account

---
+++ WithdrawAccount/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import model.entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("Enter account data:");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, name, balance, withdrawLimit);
+
+        System.out.printf("%nEnter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        account.whitdraw(amount);
+        System.out.printf("%nNew balance: $%.2f", account.getBalance());
+
+        sc.close();
+    }
+}
